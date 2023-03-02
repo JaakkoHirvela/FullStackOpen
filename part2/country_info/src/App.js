0d86413485db5175179f9940e68815fa1a79bf31
Jaakko Hirvelä <jaakko.hirvela@tuni.fi>
2023-03-02 18:36:33 +0200

    add weather tracking 2.20

--- part2/country_info/src/App.js
+++ part2/country_info/src/App.js
@@ -1,27 +1,68 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+const API_KEY = process.env.REACT_APP_API_KEY
 
-
-const SingleCountryInfo = (props) => {
+const WeatherDisplay = ({country, weather}) => {
   return (
     <div>
-      <h1>{props.name}</h1>
-      <div>
-        <p>
-          capital {props.capital}<br></br>
-          area {props.area}
-        </p>
-      </div>
-      <b>languages:</b>
-      <ul>
-        {Object.values(props.languages).map(lang => <li key={lang}>{lang}</li>)}
-      </ul>
-      <img src={props.flag.png} alt={props.flag.alt} />
+      <h1>Weather in {country.capital}</h1>
+      <p>temperature {weather.main.temp} Celcius</p>
+      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}></img>
+      <p>wind {weather.wind.speed} m/s</p>
     </div>
   )
 }
 
-const CountryInfoList = (props) => {
+const SingleCountryInfo = ({ country }) => {
+
+  const [weather, setWeather] = useState(null)
+  const lat = country.latlng[0]
+  const lng = country.latlng[1]
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}`
+  
+  useEffect(() => {
+    console.log('fetching weather..');
+    axios.get(url)
+      .then(response => {
+        setWeather(response.data)
+        console.log('effect run, weather is now', weather)    
+      })
+  }, [])
+  
+  if (weather !== null){
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <div>
+          <p>
+            capital {country.capital}<br></br>
+            area {country.area}
+          </p>
+        </div>
+        <b>languages:</b>
+        <ul>
+          {Object.values(country.languages).map(lang => <li key={lang}>{lang}</li>)}
+        </ul>
+        <img src={country.flags.png} alt={country.flags.alt} />
+        <WeatherDisplay country={country} weather={weather}/>
+      </div>
+    )
+  }
+  else{
+    return
+  }
+}
+
+const CountryInfoList = ({ countries, func }) => {
+  return (
+    countries.map(country =>
+      <div key={country.name.common} >{country.name.common}
+        <button onClick={() => func(country.name.common)}>show</button></div>
+    )
+  )
+}
+
+const CountryInfoDisplay = (props) => {
   if (props.countries !== null) {
     let countriesToShow = props.countries.filter(country => country.name.common.toLowerCase().includes(props.filter))
     let singleCountry = countriesToShow.filter(country => country.name.common.toLowerCase() === props.filter)
@@ -34,16 +75,12 @@ const CountryInfoList = (props) => {
       if (countriesToShow.length === 1) {
         const countryToShow = countriesToShow[0]
         return (
-          <SingleCountryInfo name={countryToShow.name.common} flag={countryToShow.flags}
-            languages={countryToShow.languages} capital={countryToShow.capital} area={countryToShow.area} />
+          <SingleCountryInfo country={countryToShow} />
         )
       }
       else {
         return (
-          countriesToShow.map(country =>
-            <div key={country.name.common} >{country.name.common}
-              <button onClick={() => props.onClick(country.name.common)}>show</button></div>
-          )
+          <CountryInfoList countries={countriesToShow} func={props.onClick} />
         )
       }
     }
@@ -73,7 +110,7 @@ function App() {
   return (
     <div>
       find countries <input value={countryFilter} onChange={handleCountryChange}></input>
-      <CountryInfoList countries={allCountries} filter={countryFilter}
+      <CountryInfoDisplay countries={allCountries} filter={countryFilter}
         onClick={(name) => setCountryFilter(name.toLowerCase())} />
     </div>
   );
